6c55c5dbe22fb92346c30c49226e6c5081def034
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-16 20:58:10 +0100

    Add icon's colors constant

---
+++ lib/constants/icon_colors.dart
@@ -0,0 +1,14 @@
+const List<String> iconColors = [
+  'black',
+  'blue',
+  'brown',
+  'cream',
+  'green',
+  'magenta',
+  'orange',
+  'purple',
+  'red',
+  'teal',
+  'white',
+  'yellow',
+];
