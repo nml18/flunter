6e21f93bd08ec64db101b1f83c7eb67bfe0a69cb
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-17 12:21:55 +0100

    Update GameScreen to include the Card widget

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,18 +1,36 @@
+import 'package:flunter/models/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flunter/widgets/card_widget.dart';
 
-class GameScreen extends StatelessWidget{
+class GameScreen extends StatefulWidget{
   const GameScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<StatefulWidget> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  late Game _game;  
+
+  @override
+  void initState() {
+    super.initState();
+    _game = Game(difficulty: Difficulties.easy);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text('Flunter'),
+      appBar: AppBar(
+        title: Text('Flunter'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text('Tentatives: ${_game.attempts}'),
           ),
-        ),
-        body: Center(child: Text('Cards TODO')),
+        ],
+      ),
+      body: const CardWidget(),
     );
   }
 }
