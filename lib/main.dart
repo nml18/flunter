d44c5fbb96ea58215e7b0f2675e5093d39f0a7b4
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-17 01:24:01 +0100

    Update main removed the debug label

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/game_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: GameScreen()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: GameScreen()
+  ));
 }
