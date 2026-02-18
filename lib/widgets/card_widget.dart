a22d75024bfd9f2d02e56783559ccf851ad1d026
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-18 23:45:23 +0100

    Update the card widget with onTap functionality

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -1,37 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class CardWidget extends StatefulWidget{
-  const CardWidget({super.key});
+class CardWidget extends StatefulWidget {
+  final bool isFaceUp;
+  final VoidCallback onTap;
+  final String iconName;
+
+  const CardWidget({
+    super.key,
+    required this.isFaceUp,
+    required this.onTap,
+    required this.iconName,
+  });
+
   @override
-  State<StatefulWidget> createState() => _CardWidgetState();
+  State<CardWidget> createState() => _CardWidgetState();
 }
 
-class _CardWidgetState extends State<CardWidget>{
+class _CardWidgetState extends State<CardWidget> {
   bool _isHovered = false;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MouseRegion(
       onEnter: (_) => setState(() => _isHovered = true),
       onExit: (_) => setState(() => _isHovered = false),
-      child : Card(
-        elevation: _isHovered ? 10 : 5,
-        surfaceTintColor: Colors.orange,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-          side: BorderSide(
-            color: _isHovered ? Colors.red : Colors.blueGrey,
-            width: _isHovered ? 3 : 2),
-        ),
-        child: SizedBox(
-          width: 80,
-          height: 120,
-          child: SvgPicture.asset(
-            'icons/question_marks/black.svg'
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Card(
+          elevation: _isHovered ? 10 : 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+            side: BorderSide(
+              color: Colors.blueGrey,
+              width: _isHovered ? 3 : 2,
+            ),
+          ),
+          child: SizedBox(
+            width: 80,
+            height: 120,
+            child: widget.isFaceUp
+                ? SvgPicture.asset('icons/armor_spheres/${widget.iconName}')
+                : SvgPicture.asset('icons/question_marks/black.svg'),
           ),
         ),
-      )
+      ),
     );
   }
 }
