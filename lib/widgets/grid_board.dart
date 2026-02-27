ec1e48685144040fab80b28414900b17fd0f258b
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-27 11:16:33 +0100

    Add GridBoard widget

---
+++ lib/widgets/grid_board.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flunter/models/game_card.dart';
+import 'package:flunter/widgets/card_widget.dart';
+
+class GridBoard extends StatelessWidget {
+  final List<GameCard> cards;
+  final Function(int) onCardTapped;
+
+  const GridBoard({
+    super.key,
+    required this.cards,
+    required this.onCardTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 80 / 120,
+      ),
+      itemCount: cards.length,
+      itemBuilder: (context, index) {
+        return CardWidget(
+          isFaceUp: cards[index].isFaceUp,
+          onTap: () => onCardTapped(index),
+          iconName: cards[index].iconName,
+        );
+      },
+    );
+  }
+}
