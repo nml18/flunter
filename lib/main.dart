6d0f25da0c68d47ba32cce24c5539e92a08c3464
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2026-02-28 10:31:22 +0100

    Update home set to MenuScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'screens/game_screen.dart';
+import 'screens/menu_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: GameScreen()
+    home: MenuScreen(),
   ));
 }
